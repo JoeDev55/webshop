0f1a2e03a6e56e08458aab17ca3992c42983fb33
JoeDev55 <jozsefgal0509@gmail.com>
2025-06-02 12:48:25 +0200

    search and shopping list ui

--- Frontend/src/pages/Products.js
+++ Frontend/src/pages/Products.js
@@ -6,8 +6,8 @@ import NavBar from '../NavBar';
 
 function Products() {
 
- const [products, setProducts] = useState([]);
-
+  const [products, setProducts] = useState([])
+  const [shoppingList,setShoppingList] = useState([])
   useEffect(() => {
     fetch('http://localhost:3000/productList')  // your backend endpoint
       .then(response => response.json())
@@ -15,27 +15,72 @@ function Products() {
       .catch(err => console.error('Error fetching products:', err));
   }, []);
 
+  function Add(item){
+    setShoppingList(prevList => [...prevList, item])
+  }
   return (
-    <div className="mainContainer">
+    <div className="mainContainerProducts">
       <header className="App-header">
         <NavBar/>
       </header>
+      <div className="dividerContainer">
+      <div className="searchBarContainer">
+        
+        <div className="searchBar">
+          <input type="text"></input>
+        
+          <button>
+            <p>🔍</p>
+          </button>
+        </div>
+        <div className="searchFilter">
+          <button >
+            <p>Fruits</p>
+          </button>
+          <button >
+            <p>Vegetables</p>
+          </button>
+          <button >
+            <p>Meat</p>
+          </button>
+          <button >
+            <p>Dairy</p>
+          </button>
+        </div>
+    
+
+
+
+      </div>
+      
 
       <div className="productsContainer" >
 
          <div className="products">
                 {products.map(item => (
-          <div key={item.id} className="productIndiv" >
+          <button key={item.id} className="productIndiv" onClick={()=> Add(item)}>
             <div className="productImg">
               <img  src={`http://localhost:3000/${item.img}`} alt={item.name}  />  
             </div>
           <div className="productDesc">
-            <h2>{item.name}</h2>
-            <p>Price: {item.price} Ft</p>
+            <h4>{item.name}</h4>
+            <p>{item.price} Ft</p>
           </div>
-          </div>
+          </button>
             ))}
         </div>
+      </div>
+      <div className="listContainer">
+        
+          {shoppingList.map(item=>(
+            <ul>
+          <li><p>{item.name}</p></li>
+          </ul>
+          )) 
+            
+          }
+        
+      </div>
       </div>
     </div>
   );
