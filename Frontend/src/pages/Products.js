e4b374428f1c0608c18c3e8511978e03827d02af
JoeDev55 <jozsefgal0509@gmail.com>
2025-06-03 12:31:05 +0200

    product count on list

--- Frontend/src/pages/Products.js
+++ Frontend/src/pages/Products.js
@@ -8,6 +8,7 @@ function Products() {
 
   const [products, setProducts] = useState([])
   const [shoppingList,setShoppingList] = useState([])
+  var [itemCounter,setItemCounter] = useState(1)
   useEffect(() => {
     fetch('http://localhost:3000/productList')  // your backend endpoint
       .then(response => response.json())
@@ -16,8 +17,36 @@ function Products() {
   }, []);
 
   function Add(item){
-    setShoppingList(prevList => [...prevList, item])
+    if (shoppingList.length === 0 ) {
+      setShoppingList(prevList => [...prevList, item])
+    }
+    else if(shoppingList.includes(item)){
+      console.log(item.name)
+      setItemCounter(prevCount => prevCount + 1)
+    }
   }
+  function ShopPlus(item){
+    
+      setItemCounter(prevCount => prevCount + 1)
+    
+    
+  }
+  function ShopMinus(item){
+    if (shoppingList.length >= 0) {
+      if (setItemCounter === 0) {
+        setShoppingList(prevList => prevList.filter(item => item != item));
+      }
+      setItemCounter(prevCount => prevCount - 1)
+    }
+    
+    
+  }
+  function Clear(){
+    setShoppingList([])
+    setItemCounter(0)
+  }
+  
+  
   return (
     <div className="mainContainerProducts">
       <header className="App-header">
@@ -59,7 +88,7 @@ function Products() {
          <div className="products">
                 {products.map(item => (
           <button key={item.id} className="productIndiv" onClick={()=> Add(item)}>
-            <div className="productImg">
+            <div key={item.img} className="productImg">
               <img  src={`http://localhost:3000/${item.img}`} alt={item.name}  />  
             </div>
           <div className="productDesc">
@@ -71,16 +100,32 @@ function Products() {
         </div>
       </div>
       <div className="listContainer">
-        
+        <ul>
           {shoppingList.map(item=>(
-            <ul>
-          <li><p>{item.name}</p></li>
-          </ul>
+            
+          <li>
+            <p key={item.name} className="counter">{itemCounter}x </p>
+            <p>{item.name}</p>
+            <button key={item.id} onClick={ShopMinus}>
+              <p>-</p>
+            </button>
+            <button key={item.id} onClick={ShopPlus}>
+              <p>+</p>
+            </button>
+          </li>
+          
           )) 
             
           }
+        </ul>
+        <div>
+          <button onClick={Clear}>
+            <p>Clear all</p>
+          </button>
+        </div>
         
       </div>
+      
       </div>
     </div>
   );
